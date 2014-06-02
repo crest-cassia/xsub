6bb632b601725608e10446929e226c0f77e54dc2
Yohsuke Murase <yohsuke.murase@gmail.com>
2014-06-02 14:10:58 +0900

    dummy implementation for asub

--- lib/any_scheduler.rb
+++ lib/any_scheduler.rb
@@ -1,5 +1,15 @@
+require 'pp'
 require "any_scheduler/version"
 
 module AnyScheduler
-  # Your code goes here...
+
+  extend self
+
+  def show_param
+    pp "show param"
+  end
+
+  def submit(args, parameters)
+    pp args, parameters
+  end
 end
