d81eebb270e851afa1f0dfbb44f4827a92ebb53c
Yohsuke Murase <yohsuke.murase@gmail.com>
2014-06-03 13:09:07 +0900

    implement logger

--- lib/any_scheduler/base.rb
+++ lib/any_scheduler/base.rb
@@ -1,3 +1,5 @@
+require 'logger'
+
 module AnyScheduler
 
   class Base
@@ -18,14 +20,25 @@ module AnyScheduler
       Template.render( template, parameters)
     end
 
-    def submit(job_scritps, parameters)
+    def submit(job_scritps, parameters, logger = Logger.new(STDERR) )
+      @logger = logger
+
       merged = default_parameters.merge( parameters )
+      @logger.info "Parameters: #{merged.inspect}"
 
       outputs = job_scritps.map do |job_script|
-        parent_script = render_template( merged.merge(job_file: job_script) )
-        ps_path = parent_script_path(job_script)
-        File.open( ps_path, 'w') {|f| f.write(parent_script); f.flush }
-        submit_job(ps_path)
+        begin
+          parent_script = render_template( merged.merge(job_file: job_script) )
+          ps_path = parent_script_path(job_script)
+          @logger.info "Parent script for #{job_script}: #{ps_path}"
+          File.open( ps_path, 'w') {|f| f.write(parent_script); f.flush }
+          @logger.info "Parent script has been written"
+          output = submit_job(ps_path)
+          output[:job_script] = job_script
+          output
+        rescue => ex
+          @logger.error(ex)
+        end
       end
       outputs
     end
--- lib/any_scheduler/schedulers/none.rb
+++ lib/any_scheduler/schedulers/none.rb
@@ -9,11 +9,12 @@ EOS
     PARAMETERS = {}
 
     def submit_job(script_path)
-      cmd = "nohup bash #{script_path} > /dev/null 2>&1 < /dev/null &"
+      cmd = "nohup bash #{script_path} > /dev/null 2>&1 < /dev/null & echo $!"
+      @logger.info "#{cmd} is invoked"
       output = `#{cmd}`
       raise "rc is not zero: #{output}" unless $?.to_i == 0
-      psid = `echo $!`
-      raise "echo failed" unless $?.to_i == 0
+      psid = output.lines.last.to_i
+      @logger.info "process id: #{psid}"
       {job_id: psid, output: output}
     end
   end
