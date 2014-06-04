69303b58ac8bb920591e4ebc55c2692243bb74e4
Yohsuke Murase <yohsuke.murase@gmail.com>
2014-06-04 17:34:13 +0900

    load scheduler type from ~/.any_scheduler.json

--- lib/any_scheduler.rb
+++ lib/any_scheduler.rb
@@ -15,9 +15,20 @@ module AnyScheduler
     torque: AnyScheduler::SchedulerTorque
   }
 
+  CONFIG_FILE_PATH = File.expand_path('~/.any_scheduler.json')
+
+  def self.load_scheduler
+    unless File.exist?(CONFIG_FILE_PATH)
+      $stderr.puts "Create config file #{CONFIG_FILE_PATH}"
+      raise "Config file (#{CONFIG_FILE_PATH}) not found"
+    end
+    type = JSON.load(File.open(CONFIG_FILE_PATH))["scheduler_type"].to_sym
+    scheduler(type)
+  end
+
   def self.scheduler(scheduler_type)
     key = scheduler_type.to_sym
     raise "not supported type" unless SCHEDULER_TYPE.has_key?(key)
-    SCHEDULER_TYPE[scheduler_type.to_sym]
+    SCHEDULER_TYPE[scheduler_type.to_sym].new
   end
 end
