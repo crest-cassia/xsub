b4f9151fcf4e668f3bc6945313b9cbbc76b94014
Yohsuke Murase <murase@fx02p08.aics27.riken.jp>
2014-07-28 14:08:35 +0900

    bug fix on scheduler_log file paths

--- lib/xsub/schedulers/fx10.rb
+++ lib/xsub/schedulers/fx10.rb
@@ -32,8 +32,8 @@ EOS
       FileUtils.mkdir_p(@work_dir)
       FileUtils.mkdir_p(@log_dir)
       stdout_path = File.join( File.expand_path(@log_dir), '%j.o')
-      stderr_path = File.join( File.expand_path(@log_dir), '%j.o')
-      job_stat_path = File.join( File.expand_path(@log_dir), '%i.o')
+      stderr_path = File.join( File.expand_path(@log_dir), '%j.e')
+      job_stat_path = File.join( File.expand_path(@log_dir), '%j.i')
 
       cmd = "cd #{File.expand_path(@work_dir)} && pjsub #{File.expand_path(script_path)} -o #{stdout_path} -e #{stderr_path} --spath #{job_stat_path}"
       @logger.info "cmd: #{cmd}"
