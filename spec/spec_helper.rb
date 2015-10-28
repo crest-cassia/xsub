021079b04a353210ea0f1e477e60739bb349665e
Yohsuke Murase <yohsuke.murase@gmail.com>
2015-10-28 15:22:25 +0900

    add initial spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -92,3 +92,6 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 end
+
+Dir[File.join( File.dirname(__FILE__), "../lib/**/*.rb" )].each {|f| require f}
+
---
+++ spec/unit/xsub_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe "Hello" do
+
+  it "should" do
+    expect(1+1).to eq 2
+  end
+end
+
