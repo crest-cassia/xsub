07eaa90ca74c4823b2eedbd039e8e240589258c7
Yohsuke Murase <yohsuke.murase@gmail.com>
2015-10-30 14:21:13 +0900

    add shared_example

--- spec/schedulers/k_spec.rb
+++ spec/schedulers/k_spec.rb
@@ -1,107 +1,67 @@
 require 'stringio'
+require_relative '../support/shared_examples_for_scheduler'
 
 RSpec.describe Xsub::K do
 
-  before(:each) do
-    `exit 0 > /dev/null`  # set default exit code
-  end
+  it_behaves_like "Scheduler::CONSTANTS"
 
-  it "is a descendant of Scheduler" do
-    expect( Xsub::Scheduler.descendants.include?(described_class) ).to be_truthy
-  end
 
-  it "has a string TEMPLATE" do
-    expect( described_class::TEMPLATE ).to be_a(String)
-  end
+  valid_params = [
+    {"mpi_procs" => 32,"omp_threads" => 8,"elapse"=>"2:00:00",
+     "node" => "4x4x2", "shape" => "4x4x2"},
+    {"mpi_procs" => 32,"omp_threads" => 1,"elapse"=>"2:00:00",
+     "node" => "4", "shape" => "4"}
+  ]
+  invalid_params = [
+    [
+      {"mpi_procs" => 32,"omp_threads" => 8,"elapse"=>"2:00:00",
+       "node" => "4x4x2", "shape" => "4x8"},
+      /node and shape must be/
+    ],
+    [
+      {"mpi_procs" => 32,"omp_threads" => 8,"elapse"=>"2:00:00",
+       "node" => "4x4x2", "shape" => "4x6x1"},
+      /shape must be smaller than/
+    ]
+  ]
 
-  it "has a valid format of PARAMETERS" do
-    params = described_class::PARAMETERS
-    params.each_pair do |key,val|
-      expect( key ).to match(/^[a-z][a-zA-Z_\d]*$/)
-      expect( val.keys ).to match_array [:description, :default, :format]
-      expect( val[:format] ).to be_a(String)
-    end
-  end
+  it_behaves_like "Scheduler#validate_parameters", valid_params, invalid_params
 
-  describe "#validate_parameters" do
 
-    it "does not raise an error when valid parameters are given" do
-      s = Xsub::K.new
-      params = {"mpi_procs" => 32,"omp_threads" => 8,"elapse"=>"2:00:00",
-                "node" => "4x4x2", "shape" => "4x4x2"}
-      expect {
-        s.validate_parameters(params)
-      }.to_not raise_error
-    end
-
-    it "raises an error when node and shape are incompatible" do
-      s = Xsub::K.new
-      params = {"mpi_procs" => 32,"omp_threads" => 8,"elapse"=>"2:00:00",
-                "node" => "4x4x2", "shape" => "4x8"}
-      expect {
-        s.validate_parameters(params)
-      }.to raise_error(/node and shape must be/)
-    end
-
-    it "raises an error when shape is larger than node" do
-      s = Xsub::K.new
-      params = {"mpi_procs" => 32,"omp_threads" => 8,"elapse"=>"2:00:00",
-                "node" => "4x4x2", "shape" => "4x6x1"}
-      expect {
-        s.validate_parameters(params)
-      }.to raise_error(/shape must be smaller than/)
-    end
-  end
-
-  describe "#submit_job" do
-
-    before(:each) do
-      FileUtils.mkdir_p("work_test")
-      FileUtils.mkdir_p("log_test")
-    end
-
-    after(:each) do
-      FileUtils.rm_rf("work_test")
-      FileUtils.rm_rf("log_test")
-    end
-
-    it "submits job by pjsub" do
-      s = Xsub::K.new
-      command = "cd #{Dir.pwd}/work_test && pjsub #{Dir.pwd}/job.sh -o #{Dir.pwd}/log_test/%j.o.txt -e #{Dir.pwd}/log_test/%j.e.txt --spath #{Dir.pwd}/log_test/%j.i.txt < /dev/null"
-      out = "[INFO] PJM 0000 pjsub Job 112109 submitted."
-      expect(s).to receive(:`).with(command).and_return(out)
-      out = s.submit_job("job.sh", "work_test", "log_test", StringIO.new)
-      expect( out[:job_id] ).to eq "112109"
-    end
-
-    it "prints log in the log directory" do
-      s = Xsub::K.new
-      out = "[INFO] PJM 0000 pjsub Job 112109 submitted."
-      allow(s).to receive(:`).and_return(out)
-      log = StringIO.new
-      s.submit_job("job.sh", "work_test", "log_test", log)
-      expect( log.string ).to_not be_empty
-    end
-
-    it "raises an error when staging option is invalid" do
-      s = Xsub::K.new
-      out = <<EOS
+  submit_test_ok_cases = [
+    {
+      command: "cd #{Dir.pwd}/work_test && pjsub #{Dir.pwd}/job.sh -o #{Dir.pwd}/log_test/%j.o.txt -e #{Dir.pwd}/log_test/%j.e.txt --spath #{Dir.pwd}/log_test/%j.i.txt < /dev/null",
+      out: "[INFO] PJM 0000 pjsub Job 112109 submitted.",
+      rc: 0,
+      job_id: "112109"
+    }
+  ]
+  submit_test_ng_cases = [
+    {
+      command: nil,   # do not check command
+      out: <<EOS,
 [ERR.] PJM 0007 pjsub Staging option error (3)
 Refer to the staging information file. (J5333b14881e31ebcd2000001.sh.s2366652)
 EOS
-      allow(s).to receive(:`).and_return(out)
-      expect {
-        s.submit_job("job.sh", "work_test", "log_test", StringIO.new)
-      }.to raise_error(/staging option error/)
-    end
-  end
+      rc: 0,
+      error: /staging option error/
+    },
+    {
+      command: nil,
+      out: nil,
+      rc: 1,
+      error: /rc is not zero/
+    }
+  ]
 
-  describe "#status" do
+  it_behaves_like "Scheduler#submit_job", submit_test_ok_cases, submit_test_ng_cases
 
-    it "returns :queued status by qstat" do
-      s = Xsub::K.new
-      command = "pjstat 112111"
-      stat = <<EOS
+
+  status_test_cases = [
+    {
+      job_id: "112111",
+      command: "pjstat 112111",
+      out: <<EOS,
         ACCEPT QUEUED  STGIN  READY RUNING RUNOUT STGOUT   HOLD  ERROR   TOTAL
        0      0      0      0      1      0      0      0      0       1
 s      0      0      0      0      1      0      0      0      0       1
@@ -109,15 +69,13 @@ s      0      0      0      0      1      0      0      0      0       1
 JOB_ID     JOB_NAME   MD ST  USER     START_DATE      ELAPSE_LIM NODE_REQUIRE
 112111     test.sh    NM QUE test     10/29 18:42:44  0024:00:00 1
 EOS
-      expect(s).to receive(:`).with(command).and_return(stat)
-      out = s.status("112111")
-      expect( out[:status] ).to eq :queued
-    end
-
-    it "returns :running status by qstat" do
-      s = Xsub::K.new
-      command = "pjstat 112111"
-      stat = <<EOS
+      rc: 0,
+      status: :queued
+    },
+    {
+      job_id: "112111",
+      command: "pjstat 112111",
+      out: <<EOS,
   ACCEPT QUEUED  STGIN  READY RUNING RUNOUT STGOUT   HOLD  ERROR   TOTAL
        0      0      0      0      1      0      0      0      0       1
 s      0      0      0      0      1      0      0      0      0       1
@@ -125,15 +83,13 @@ s      0      0      0      0      1      0      0      0      0       1
 JOB_ID     JOB_NAME   MD ST  USER     START_DATE      ELAPSE_LIM NODE_REQUIRE
 112111     test.sh    NM RUN test     10/29 18:42:44  0024:00:00 1
 EOS
-      expect(s).to receive(:`).with(command).and_return(stat)
-      out = s.status("112111")
-      expect( out[:status] ).to eq :running
-    end
-
-    it "returns :finished status by pjstat" do
-      s = Xsub::K.new
-      command = "pjstat 112111"
-      stat = <<EOS
+      rc: 0,
+      status: :running
+    },
+    {
+      job_id: "112111",
+      command: "pjstat 112111",
+      out: <<EOS,
   ACCEPT QUEUED  STGIN  READY RUNING RUNOUT STGOUT   HOLD  ERROR   TOTAL
        0      0      0      0      1      0      0      0      0       1
 s      0      0      0      0      1      0      0      0      0       1
@@ -141,33 +97,25 @@ s      0      0      0      0      1      0      0      0      0       1
 JOB_ID     JOB_NAME   MD ST  USER     START_DATE      ELAPSE_LIM NODE_REQUIRE
 112111     test.sh    NM EXT test     10/29 18:42:44  0024:00:00 1
 EOS
-      expect(s).to receive(:`).with(command).and_return(stat)
-      out = s.status("112111")
-      expect( out[:status] ).to eq :finished
-    end
-
-    it "returns :finished status by qstat when job_id is not found" do
-      s = Xsub::K.new
-      command = "pjstat 112111"
-      stat = <<EOS
+      rc: 0,
+      status: :finished
+    },
+    {
+      job_id: "112111",
+      command: "pjstat 112111",
+      out: <<EOS,
   ACCEPT QUEUED  STGIN  READY RUNING RUNOUT STGOUT   HOLD  ERROR   TOTAL
        0      0      0      0      0      0      0      0      0       0
 s      0      0      0      0      0      0      0      0      0       0
 EOS
-      expect(s).to receive(:`).with(command).and_return(stat)
-      out = s.status("112111")
-      expect( out[:status] ).to eq :finished
-    end
-  end
+      rc: 0,
+      status: :finished
+    }
+  ]
 
-  describe "#all_status" do
+  it_behaves_like "Scheduler#status", status_test_cases
 
-    it "returns status in string" do
-      s = Xsub::K.new
-      expect(s).to receive(:`).with("pjstat").and_return("abc")
-      expect( s.all_status ).to eq "abc"
-    end
-  end
+  it_behaves_like "Scheduler#all_status", "pjstat"
 
   describe "#delete" do
 
---
+++ spec/support/shared_examples_for_scheduler.rb
@@ -0,0 +1,142 @@
+require 'stringio'
+
+RSpec.shared_examples "Scheduler::CONSTANTS" do
+
+  it "is a descendant of Scheduler" do
+    expect( Xsub::Scheduler.descendants.include?(described_class) ).to be_truthy
+  end
+
+  it "has a string TEMPLATE" do
+    expect( described_class::TEMPLATE ).to be_a(String)
+  end
+
+  it "has a valid format of PARAMETERS" do
+    params = described_class::PARAMETERS
+    params.each_pair do |key,val|
+      expect( key ).to match(/^[a-z][a-zA-Z_\d]*$/)
+      expect( val.keys ).to match_array [:description, :default, :format]
+      expect( val[:format] ).to be_a(String)
+    end
+  end
+end
+
+RSpec.shared_examples "Scheduler#validate_parameters" do |valid_prms, invalid_prms|
+
+  describe "#validate_parameters" do
+
+    describe "valid cases" do
+      valid_prms.each_with_index do |valid_param,idx|
+
+        it "does not raise an error when valid parameters are given (#{idx+1})" do
+          s = described_class.new
+          expect {
+            s.validate_parameters( valid_param )
+          }.to_not raise_error
+        end
+      end
+    end
+
+    describe "invalid cases" do
+      invalid_prms.each_with_index do |(invalid_param,msg),idx|
+
+        it "raises an error with invalid parameters (#{idx+1})" do
+          s = described_class.new
+          expect {
+            s.validate_parameters( invalid_param )
+          }.to raise_error(msg)
+        end
+      end
+    end
+  end
+end
+
+RSpec.shared_examples "Scheduler#submit_job" do |ok_cases,ng_cases|
+
+  describe "#submit_job" do
+
+    before(:each) do
+      FileUtils.mkdir_p("work_test")
+      FileUtils.mkdir_p("log_test")
+    end
+
+    after(:each) do
+      FileUtils.rm_rf("work_test")
+      FileUtils.rm_rf("log_test")
+    end
+
+    ok_cases.each_with_index do |ok_case,idx|
+
+      it "submits job correctly (#{idx+1})" do
+        s = described_class.new
+        cmd = ok_case[:command]
+        out = ok_case[:out]
+        rc = ok_case[:rc]
+        job_id = ok_case[:job_id]
+        expect(s).to receive(:`) {|arg|
+          expect(arg).to eq cmd if cmd
+          `exit #{rc} > /dev/null` if rc
+          out
+        }
+        ret = s.submit_job("job.sh", "work_test", "log_test", StringIO.new)
+        expect( ret[:job_id] ).to eq job_id
+      end
+    end
+
+    ng_cases.each_with_index do |ng_case,idx|
+
+      it "raises an error when submission fails (#{idx+1})" do
+        s = described_class.new
+        cmd = ng_case[:command]
+        out = ng_case[:out]
+        rc = ng_case[:rc]
+        error = ng_case[:error]
+        expect(s).to receive(:`) {|arg|
+          expect(arg).to eq cmd if cmd
+          `exit #{rc} > /dev/null` if rc
+          out
+        }
+        expect {
+          s.submit_job("job.sh", "work_test", "log_test", StringIO.new)
+        }.to raise_error(error)
+      end
+    end
+  end
+end
+
+RSpec.shared_examples "Scheduler#status" do |cases|
+
+  describe "#status" do
+
+    cases.each_with_index do |test_case,idx|
+
+      it "returns status (#{idx+1})" do
+        cmd = test_case[:command]
+        rc = test_case[:rc]
+        job_id = test_case[:job_id]
+
+        s = described_class.new
+        expect(s).to receive(:`) {|arg|
+          expect(arg).to eq cmd if cmd
+          `exit #{rc} > /dev/null` if rc
+          test_case[:out]
+        }
+        ret = s.status( test_case[:job_id] )
+        expect( ret[:status] ).to eq test_case[:status]
+      end
+    end
+  end
+end
+
+RSpec.shared_examples "Scheduler#all_status" do |expected_cmd|
+
+  describe "#all_status" do
+    
+    it "return status in string" do
+      s = described_class.new
+      expect(s).to receive(:`).with(expected_cmd).and_return("abc")
+      expect( s.all_status ).to eq "abc"
+    end
+  end
+end
+
+RSpec.shared_examples "Scheduler#delete"
