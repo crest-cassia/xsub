8920e090f2b994d7882f7da431c8e80b1774fa74
Itsuki Noda <i.noda@aist.go.jp>
2018-12-14 17:56:41 +0900

    add scheduler and its spec for ABCI @ AIST.

---
+++ lib/schedulers/abci.rb
@@ -0,0 +1,83 @@
+require 'date'
+require File.expand_path(File.dirname(__FILE__)+'/../scheduler')
+
+module Xsub
+
+  class Abci < Scheduler
+
+    TEMPLATE = <<EOS
+#!/bin/bash
+#$ -l <%= resource_type_num %>
+#$ -l h_rt=<%= walltime %>
+#$ -N <%= name_job %>
+#$ -p <%= priority %>
+#$ -cwd
+cd <%= File.expand_path(_work_dir) %>
+LANG=C
+. <%= _job_file %>
+EOS
+
+    PARAMETERS = {
+      "resource_type_num" =>  { :description => "(resource_type)=(num)", :default => "rt_F=1", :format => '$'},
+      "name_job" => { :description => "name of job", :default => "oacis_job", :format => '$'},
+      "group" => { :description => "user group", :default => "group_name", :format => '$'},
+      "priority" => { :description => "priority", :default => 0, :format => '^[0-9]\d*$'},
+      "walltime" => { :description => "Limit on elapsed time", :default => "0:01:00", :format => '^\d+:\d{2}:\d{2}$'},
+      
+      "mpi_procs" => { :description => "MPI process", :default => 1, :format => '^[1-9]\d*$'},
+      "omp_threads" => { :description => "OMP threads", :default => 1, :format => '^[1-9]\d*$'}
+    }
+    
+    def validate_parameters(prm)
+# groupをsubmit_job()で参照したいため無理やり．submit_job()の前に必ず呼び出される．
+    	@@group = prm["group"];
+    end
+
+    def submit_job(script_path, work_dir, log_dir, log)
+      cmd = "cd #{File.expand_path(work_dir)} && qsub -g #{@@group} -o #{File.expand_path(log_dir)}/execute.log -e #{File.expand_path(log_dir)}/error.log #{File.expand_path(script_path)}"
+      log.puts "cmd: #{cmd}", "time: #{DateTime.now}"
+      output = `#{cmd}`
+      unless $?.to_i == 0
+        log.puts "rc is not zero: #{output}"
+        raise "rc is not zero: #{output}, #{File.expand_path(work_dir)}"
+      end
+      job_id = output.lines.to_a.last.split[2]
+      log.puts "job_id: #{job_id}"
+      {:job_id => job_id, :raw_output => output.lines.map(&:chomp).to_a }
+    end
+
+    def status(job_id)
+      cmd = "qstat | grep #{job_id}"
+      output = `#{cmd}`
+      if $?.to_i == 0
+        status = case output.lines.to_a.last.split[4]
+        when /qw|hqw/
+          :queued
+        when /r/
+          :running
+        when /e/
+          :failed
+        else
+          raise "unknown output: #{output}"
+        end
+      else
+        status = :finished
+      end
+      { :status => status, :raw_output => output.lines.map(&:chomp).to_a }
+    end
+
+    def all_status
+      cmd = "qstat -g c"
+      output = `#{cmd}`
+      output
+    end
+
+    def delete(job_id)
+      cmd = "qdel #{job_id}"
+      output = `#{cmd}`
+      raise "failed to delete job: #{job_id}" unless $?.to_i == 0
+      output
+    end
+  end
+end
+
---
+++ spec/schedulers/abci_spec.rb
@@ -0,0 +1,101 @@
+require File.join( File.dirname(__FILE__), '../support/shared_examples_for_scheduler')
+
+RSpec.describe Xsub::Abci do
+
+  it_behaves_like "Scheduler::CONSTANTS"
+
+
+  valid_params = [
+    {"mpi_procs" => 4, "omp_threads" => 8, "ppn" => 8}
+  ]
+  invalid_params = [
+    [
+      {"mpi_procs" => 4, "omp_threads" => 8, "ppn" => 6},
+      /must be a multiple of ppn/
+    ]
+  ]
+
+  it_behaves_like "Scheduler#validate_parameters", valid_params, invalid_params
+
+  submit_test_ok_cases = [
+    {
+      :command => "qsub #{Dir.pwd}/job.sh -d #{Dir.pwd}/work_test -o #{Dir.pwd}/log_test -e #{Dir.pwd}/log_test",
+      :out => "19352.localhost",
+      :rc => 0,
+      :job_id => "19352"
+    }
+  ]
+  submit_test_ng_cases = [
+    {
+      :command => nil,
+      :out => nil,
+      :rc => 1,
+      :error => /rc is not zero/
+    }
+  ]
+
+  it_behaves_like "Scheduler#submit_job", submit_test_ok_cases, submit_test_ng_cases
+
+
+    status_test_cases = [
+    {
+      :job_id => "19352",
+      :command => "qstat 19352",
+      :out => <<EOS,
+Job id                    Name             User            Time Use S Queue
+------------------------- ---------------- --------------- -------- - -----
+19352.localhost           job.sh           test_user              0 Q batch
+EOS
+      :rc => 0,
+      :status => :queued
+    },
+    {
+      :job_id => "19352",
+      :command => "qstat 19352",
+      :out => <<EOS,
+Job id                    Name             User            Time Use S Queue
+------------------------- ---------------- --------------- -------- - -----
+19352.localhost           job.sh           test_user              0 R batch
+EOS
+      :rc => 0,
+      :status => :running
+    },
+    {
+      :job_id => "19352",
+      :command => "qstat 19352",
+      :out => <<EOS,
+Job id                    Name             User            Time Use S Queue
+------------------------- ---------------- --------------- -------- - -----
+19352.localhost           job.sh           test_user              0 C batch
+EOS
+      :rc => 0,
+      :status => :finished
+    },
+    {
+      :job_id => "19352",
+      :command => "qstat 19352",
+      :out => <<EOS,
+EOS
+      :rc => 153,
+      :status => :finished
+    },
+    {
+      :job_id => "19352",
+      :command => "qstat 19352",
+      :out => <<EOS,
+Job ID                    Name             User            Time Use S Queue
+------------------------- ---------------- --------------- -------- - -----
+19352.localhost           ...b0000_xsub.sh test_user       02:13:28 E batch
+EOS
+      :rc => 0,
+      :status => :running
+    }
+    ]
+
+  it_behaves_like "Scheduler#status", status_test_cases
+
+  it_behaves_like "Scheduler#all_status", "qstat && pbsnodes -a"
+
+  it_behaves_like "Scheduler#delete", "qdel"
+end
+
